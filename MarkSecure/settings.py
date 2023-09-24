55c5c0a0c18b34eeadec93fede306958e80642ca
M <50911439+mPenTests@users.noreply.github.com>
2023-09-24 10:49:00 +0200

    Added Postgres DB

--- MarkSecure/settings.py
+++ MarkSecure/settings.py
@@ -85,11 +85,15 @@ WSGI_APPLICATION = 'MarkSecure.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': 'postgresql://postgres:NV8Midqrp2RuX3PmKPy9@containers-us-west-135.railway.app:5563/railway',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'NV8Midqrp2RuX3PmKPy9',
+        'HOST': 'containers-us-west-135.railway.app',
+        'PORT': 5563,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
